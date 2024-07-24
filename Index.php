552e104abebede754fcbd55316be059e78862468
Nicolò Pincelli <pincelli.n@gmail.com>
2024-07-24 16:40:17 +0200

    start

---
+++ Index.php
@@ -0,0 +1,92 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+
+   
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<title>Table php</title>
+
+</head>
+
+<body>
+
+    <div class="container m-4 p-4">
+        <table class="table">
+            <thead>
+
+
+                <tr>
+                    <?php foreach($hotels[0] as $key=>$value):?>
+                    <th scope="col"><?php echo $key?></th>
+                    <?php endforeach;?>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($hotels as $hotel):   ?>
+                <tr>
+                    <th scope="row"><?php echo $hotel['name']?></th>
+                    <td><?php  echo $hotel['description'] ?></td>
+                    <td><?php  echo $hotel['parking'] ?></td>
+                    <td><?php  echo $hotel['vote'] ?></td>
+                    <td><?php  echo $hotel['distance_to_center'] ?></td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+
+
+        </table>
+    </div>
+</body>
+
+</html>
